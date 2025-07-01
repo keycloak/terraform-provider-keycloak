a68f59b53e13598f6267fdf230963ea5b807b813
Christopher Svensson <christopher.svensson@stegra.com>
2025-07-01 13:55:57 +0200

    Adjusted auto-formatted imports

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -4,15 +4,14 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
-	"os"
-	"testing"
-	"time"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/meta"
 	"github.com/keycloak/terraform-provider-keycloak/keycloak"
+	"log"
+	"os"
+	"testing"
+	"time"
 )
 
 var testAccProviderFactories map[string]func() (*schema.Provider, error)
