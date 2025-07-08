1d187b669756f6d786ad166ca61ed25b94a01d16
elshirak <el.shirak@proton.me>
2025-07-08 08:44:29 +0000

    Cosmetic changes
    Signed-off-by: Shirak Elbakyan <shirak.elbakyan@bostongene.com>

--- provider/resource_keycloak_openid_client_authorization_role_policy_test.go
+++ provider/resource_keycloak_openid_client_authorization_role_policy_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strings"
 	"testing"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
