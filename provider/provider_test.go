2304059afb98d644f03c0ec547d749b61aace22c
Mikhail Putilov <Mikhail.Putilov@dinape.com>
2025-10-25 13:38:02 +0200

    Do not delete the "account" client in the acceptance test because it is undeletable (KeyCloak returns http status 400)
    Terraform acceptance test library simply can't handle this situation. One way that I found to overcome this is to replace the `DeleteContext` function in acceptance tests only.
    
    I believe we must not alter the behavior of the standard response of KeyCloak when a client is deleted, nor hide that fact that KeyCloak response is 400 in the production code.
    
    Signed-off-by: Mikhail Putilov <Mikhail.Putilov@dinape.com>

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/meta"
@@ -41,6 +42,15 @@ func init() {
 		panic(err)
 	}
 	testAccProvider = KeycloakProvider(keycloakClient)
+
+	testAccProvider.ResourcesMap["keycloak_openid_client"].DeleteContext = func(ctx context.Context, data *schema.ResourceData, i interface{}) diag.Diagnostics {
+		if data.State().Attributes["client_id"] == "account" {
+			return diag.Diagnostics{}
+		} else {
+			return resourceKeycloakOpenidClientDelete(ctx, data, i)
+		}
+	}
+
 	testAccProviderFactories = map[string]func() (*schema.Provider, error){
 		"keycloak": func() (*schema.Provider, error) {
 			return testAccProvider, nil
