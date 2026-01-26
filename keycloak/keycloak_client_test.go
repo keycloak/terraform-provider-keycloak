040ea5cf5e4335b0719178e30deeb03edfbe973c
Sebastian Łaskawiec <sebastian.laskawiec@gmail.com>
2026-01-26 10:53:54 +0100

    Signed JWT authentication (#1441)
    * Signed JWT authentication
    
    Signed-off-by: Sebastian Łaskawiec <sebastian.laskawiec@gmail.com>
    
    * Removed not needed dependency
    
    Signed-off-by: Sebastian Łaskawiec <sebastian.laskawiec@gmail.com>
    
    ---------
    
    Signed-off-by: Sebastian Łaskawiec <sebastian.laskawiec@gmail.com>
    Co-authored-by: Sebastian Schuster <sebastian.schuster@bosch.com>

--- keycloak/keycloak_client_test.go
+++ keycloak/keycloak_client_test.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/keycloak/terraform-provider-keycloak/helper"
 )
 
@@ -73,6 +74,11 @@ func TestAccKeycloakClientConnectHttpsMtlsAuth(t *testing.T) {
 		t.Fatalf("%s", err)
 	}
 
+	// skip test if running 26.0 or lower
+	if v, _ := keycloakClient.VersionIsLessThanOrEqualTo(ctx, Version_26); v {
+		t.Skip("We only test Keycloak > 26.0")
+	}
+
 	keycloakUrl := os.Getenv("KEYCLOAK_URL")
 	if !strings.HasPrefix(keycloakUrl, "https://") {
 		// only run tests for https URL
@@ -82,6 +88,85 @@ func TestAccKeycloakClientConnectHttpsMtlsAuth(t *testing.T) {
 	// then try again to connect with Keycloak but this time via https with mtls client auth
 	mtlsKeycloakClient, err := NewKeycloakClient(ctx, keycloakUrl, "", os.Getenv("KEYCLOAK_ADMIN_URL"), os.Getenv("KEYCLOAK_CLIENT_ID"), os.Getenv("KEYCLOAK_CLIENT_SECRET"), os.Getenv("KEYCLOAK_REALM"), os.Getenv("KEYCLOAK_USER"), os.Getenv("KEYCLOAK_PASSWORD"), os.Getenv("KEYCLOAK_ACCESS_TOKEN"), "", "", os.Getenv("KEYCLOAK_JWT_TOKEN"), os.Getenv("KEYCLOAK_JWT_TOKEN_FILE"), true, clientTimeout, os.Getenv("KEYCLOAK_TLS_CA_CERT"), true, os.Getenv("KEYCLOAK_TLS_CLIENT_CERT"), os.Getenv("KEYCLOAK_TLS_CLIENT_KEY"), "", false, map[string]string{})
 	keycloakClientChecks(t, err, mtlsKeycloakClient, ctx)
+}
+
+// Some actions, such as creating a realm, require a refresh
+// before a GET can be performed on that realm
+//
+// This test ensures that, after creating a realm and performing
+// a GET, the access token and refresh token have changed
+//
+// Any action that returns a 403 or a 401 could be used for this test
+// Creating a realm is just the only one I'm aware of
+//
+// This appears to have been fixed as of Keycloak 12.x
+func TestAccKeycloakApiClientRefresh(t *testing.T) {
+	ctx := context.Background()
+
+	helper.CheckRequiredEnvironmentVariables(t)
+
+	clientTimeout := checkClientTimeout(t)
+
+	keycloakClient, err := NewKeycloakClient(ctx, os.Getenv("KEYCLOAK_URL"), "", os.Getenv("KEYCLOAK_ADMIN_URL"), os.Getenv("KEYCLOAK_CLIENT_ID"), os.Getenv("KEYCLOAK_CLIENT_SECRET"), os.Getenv("KEYCLOAK_REALM"), os.Getenv("KEYCLOAK_USER"), os.Getenv("KEYCLOAK_PASSWORD"), os.Getenv("KEYCLOAK_ACCESS_TOKEN"), "", "", os.Getenv("KEYCLOAK_JWT_TOKEN"), os.Getenv("KEYCLOAK_JWT_TOKEN_FILE"), true, clientTimeout, os.Getenv("KEYCLOAK_TLS_CA_CERT"), false, os.Getenv("KEYCLOAK_TLS_CLIENT_CERT"), os.Getenv("KEYCLOAK_TLS_CLIENT_KEY"), "", false, map[string]string{
+		"foo": "bar",
+	})
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	// skip test if running 12.x or greater
+	if v, _ := keycloakClient.VersionIsGreaterThanOrEqualTo(ctx, Version_12); v {
+		t.Skip()
+	}
+
+	realmName := "terraform-" + acctest.RandString(10)
+	realm := &Realm{
+		Realm: realmName,
+		Id:    realmName,
+	}
+
+	err = keycloakClient.NewRealm(ctx, realm)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	var oldAccessToken, oldRefreshToken, oldTokenType string
+
+	// A following GET for this realm will result in a 403, so we should save the current access and refresh token
+	if keycloakClient.clientCredentials.GrantType == "client_credentials" {
+		oldAccessToken = keycloakClient.clientCredentials.AccessToken
+		oldRefreshToken = keycloakClient.clientCredentials.RefreshToken
+		oldTokenType = keycloakClient.clientCredentials.TokenType
+	}
+
+	_, err = keycloakClient.GetRealm(ctx, realmName) // This should not fail since it will automatically refresh and try again
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	// Clean up - the realm doesn't need to exist in order for us to assert against the refreshed tokens
+	err = keycloakClient.DeleteRealm(ctx, realmName)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	if keycloakClient.clientCredentials.GrantType == "client_credentials" {
+		newAccessToken := keycloakClient.clientCredentials.AccessToken
+		newRefreshToken := keycloakClient.clientCredentials.RefreshToken
+		newTokenType := keycloakClient.clientCredentials.TokenType
+
+		if oldAccessToken == newAccessToken {
+			t.Fatalf("expected access token to update after refresh")
+		}
+
+		if oldRefreshToken == newRefreshToken {
+			t.Fatalf("expected refresh token to update after refresh")
+		}
+
+		if oldTokenType != newTokenType {
+			t.Fatalf("expected token type to remain the same after refresh")
+		}
+	}
 }
 
 func checkClientTimeout(t *testing.T) int {
