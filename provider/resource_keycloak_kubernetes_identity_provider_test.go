54d14c7625440509d614a4142b84befcd8185d51
Laureat Grepi <laureatgrepi@hotmail.com>
2026-02-19 15:22:07 +0100

    Add tests
    Signed-off-by: Laureat Grepi <laureatgrepi@hotmail.com>

---
+++ provider/resource_keycloak_kubernetes_identity_provider_test.go
@@ -0,0 +1,110 @@
+package provider
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/keycloak/terraform-provider-keycloak/keycloak"
+)
+
+func TestAccKeycloakKubernetesIdentityProvider_basic(t *testing.T) {
+	t.Parallel()
+
+	kubernetesName := acctest.RandomWithPrefix("tf-acc")
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testAccProviderFactories,
+		PreCheck:          func() { testAccPreCheck(t) },
+		CheckDestroy:      testAccCheckKeycloakKubernetesIdentityProviderDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testKeycloakKubernetesIdentityProvider_basic(kubernetesName, "https://example.com/issuer"),
+				Check:  testAccCheckKeycloakKubernetesIdentityProviderExists("keycloak_kubernetes_identity_provider.kubernetes"),
+			},
+		},
+	})
+}
+
+func TestAccKeycloakKubernetesIdentityProvider_insecureIssuer(t *testing.T) {
+	t.Parallel()
+
+	kubernetesName := acctest.RandomWithPrefix("tf-acc")
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testAccProviderFactories,
+		PreCheck:          func() { testAccPreCheck(t) },
+		CheckDestroy:      testAccCheckKeycloakKubernetesIdentityProviderDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config:      testKeycloakKubernetesIdentityProvider_basic(kubernetesName, "http://example.com/issuer"),
+				ExpectError: regexp.MustCompile("The url \\[issuer\\] requires secure connections"),
+			},
+		},
+	})
+}
+
+func testKeycloakKubernetesIdentityProvider_basic(alias, issuer string) string {
+	return fmt.Sprintf(`
+		data "keycloak_realm" "realm" {
+			realm = "%s"
+		}
+
+		resource "keycloak_kubernetes_identity_provider" "kubernetes" {
+			realm  = data.keycloak_realm.realm.id
+			alias  = "%s"
+			issuer = "%s"
+		}
+			`, testAccRealm.Realm, alias, issuer)
+}
+
+func testAccCheckKeycloakKubernetesIdentityProviderExists(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		_, err := getKeycloakKubernetesIdentityProviderFromState(s, resourceName)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func getKeycloakKubernetesIdentityProviderFromState(s *terraform.State, resourceName string) (*keycloak.IdentityProvider, error) {
+	rs, ok := s.RootModule().Resources[resourceName]
+	if !ok {
+		return nil, fmt.Errorf("resource not found: %s", resourceName)
+	}
+
+	realm := rs.Primary.Attributes["realm"]
+	alias := rs.Primary.Attributes["alias"]
+
+	kubernetes, err := keycloakClient.GetIdentityProvider(testCtx, realm, alias)
+	if err != nil {
+		return nil, fmt.Errorf("error getting kubernetes identity provider config with alias %s: %s", alias, err)
+	}
+
+	return kubernetes, nil
+}
+
+func testAccCheckKeycloakKubernetesIdentityProviderDestroy() resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		for _, rs := range s.RootModule().Resources {
+			if rs.Type != "keycloak_kubernetes_identity_provider" {
+				continue
+			}
+
+			id := rs.Primary.ID
+			realm := rs.Primary.Attributes["realm"]
+
+			kubernetes, _ := keycloakClient.GetIdentityProvider(testCtx, realm, id)
+			if kubernetes != nil {
+				return fmt.Errorf("kubernetes config with id %s still exists", id)
+			}
+		}
+
+		return nil
+	}
+}
