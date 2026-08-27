42e0e1441ea8c3a6192a5411b925c904c875ba4d
Toon van Benthem <Toon.vanBenthem@theapsgroup.com>
2026-08-27 09:01:30 +0200

    feat: add organization_id support to keycloak_group_roles
    Keycloak 26.6.0+ requires organization groups to be managed via the
    Organization API. The regular groups API rejects requests with:
    'Cannot manage organization related group via non Organization API.'
    
    This adds an optional organization_id attribute to keycloak_group_roles.
    When set, all role mapping operations use the Organization API path:
    /realms/{realm}/organizations/{orgId}/groups/{groupId}/role-mappings/...
    
    Changes:
    - keycloak/group_role_mappings.go: add Organization variants of all
      role mapping functions, original functions delegate with empty orgId
    - provider/resource_keycloak_group_roles.go: add organization_id schema
      attribute, use GetOrganizationGroup and org-aware role mapping functions
    - docs/resources/group_roles.md: add organization group example, update
      argument reference and import format
    
    Signed-off-by: Toon van Benthem <Toon.vanBenthem@theapsgroup.com>

--- keycloak/group_role_mappings.go
+++ keycloak/group_role_mappings.go
@@ -5,9 +5,20 @@ import (
 	"fmt"
 )
 
-func (keycloakClient *KeycloakClient) GetGroupRoleMappings(ctx context.Context, realmId string, userId string) (*RoleMapping, error) {
+func groupRoleMappingsUrl(realmId, organizationId, groupId string) string {
+	if organizationId != "" {
+		return fmt.Sprintf("/realms/%s/organizations/%s/groups/%s/role-mappings", realmId, organizationId, groupId)
+	}
+	return fmt.Sprintf("/realms/%s/groups/%s/role-mappings", realmId, groupId)
+}
+
+func (keycloakClient *KeycloakClient) GetGroupRoleMappings(ctx context.Context, realmId string, groupId string) (*RoleMapping, error) {
+	return keycloakClient.GetOrganizationGroupRoleMappings(ctx, realmId, "", groupId)
+}
+
+func (keycloakClient *KeycloakClient) GetOrganizationGroupRoleMappings(ctx context.Context, realmId, organizationId, groupId string) (*RoleMapping, error) {
 	var roleMapping *RoleMapping
-	err := keycloakClient.get(ctx, fmt.Sprintf("/realms/%s/groups/%s/role-mappings", realmId, userId), &roleMapping, nil)
+	err := keycloakClient.get(ctx, groupRoleMappingsUrl(realmId, organizationId, groupId), &roleMapping, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -16,25 +27,37 @@ func (keycloakClient *KeycloakClient) GetGroupRoleMappings(ctx context.Context, 
 }
 
 func (keycloakClient *KeycloakClient) AddRealmRolesToGroup(ctx context.Context, realmId, groupId string, roles []*Role) error {
-	_, _, err := keycloakClient.post(ctx, fmt.Sprintf("/realms/%s/groups/%s/role-mappings/realm", realmId, groupId), roles)
+	return keycloakClient.AddRealmRolesToOrganizationGroup(ctx, realmId, "", groupId, roles)
+}
 
+func (keycloakClient *KeycloakClient) AddRealmRolesToOrganizationGroup(ctx context.Context, realmId, organizationId, groupId string, roles []*Role) error {
+	_, _, err := keycloakClient.post(ctx, fmt.Sprintf("%s/realm", groupRoleMappingsUrl(realmId, organizationId, groupId)), roles)
 	return err
 }
 
 func (keycloakClient *KeycloakClient) AddClientRolesToGroup(ctx context.Context, realmId, groupId, clientId string, roles []*Role) error {
-	_, _, err := keycloakClient.post(ctx, fmt.Sprintf("/realms/%s/groups/%s/role-mappings/clients/%s", realmId, groupId, clientId), roles)
+	return keycloakClient.AddClientRolesToOrganizationGroup(ctx, realmId, "", groupId, clientId, roles)
+}
 
+func (keycloakClient *KeycloakClient) AddClientRolesToOrganizationGroup(ctx context.Context, realmId, organizationId, groupId, clientId string, roles []*Role) error {
+	_, _, err := keycloakClient.post(ctx, fmt.Sprintf("%s/clients/%s", groupRoleMappingsUrl(realmId, organizationId, groupId), clientId), roles)
 	return err
 }
 
 func (keycloakClient *KeycloakClient) RemoveRealmRolesFromGroup(ctx context.Context, realmId, groupId string, roles []*Role) error {
-	err := keycloakClient.delete(ctx, fmt.Sprintf("/realms/%s/groups/%s/role-mappings/realm", realmId, groupId), roles)
+	return keycloakClient.RemoveRealmRolesFromOrganizationGroup(ctx, realmId, "", groupId, roles)
+}
 
+func (keycloakClient *KeycloakClient) RemoveRealmRolesFromOrganizationGroup(ctx context.Context, realmId, organizationId, groupId string, roles []*Role) error {
+	err := keycloakClient.delete(ctx, fmt.Sprintf("%s/realm", groupRoleMappingsUrl(realmId, organizationId, groupId)), roles)
 	return err
 }
 
 func (keycloakClient *KeycloakClient) RemoveClientRolesFromGroup(ctx context.Context, realmId, groupId, clientId string, roles []*Role) error {
-	err := keycloakClient.delete(ctx, fmt.Sprintf("/realms/%s/groups/%s/role-mappings/clients/%s", realmId, groupId, clientId), roles)
+	return keycloakClient.RemoveClientRolesFromOrganizationGroup(ctx, realmId, "", groupId, clientId, roles)
+}
 
+func (keycloakClient *KeycloakClient) RemoveClientRolesFromOrganizationGroup(ctx context.Context, realmId, organizationId, groupId, clientId string, roles []*Role) error {
+	err := keycloakClient.delete(ctx, fmt.Sprintf("%s/clients/%s", groupRoleMappingsUrl(realmId, organizationId, groupId), clientId), roles)
 	return err
 }
